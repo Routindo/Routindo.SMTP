d4534201f457f4cfbde1d4b50a8f4e568ead5830
Younes Cheikh <younes.cheikh@gmail.com>
2021-02-28 22:50:26 +0100

    Updated Uris links to support and documentation

--- Source/Umator.Plugins.Email/AssemblyInfo.cs
+++ Source/Umator.Plugins.Email/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 
 [assembly: UmatorPlugin(
     "76B83A8C-011A-4A57-A5D2-E41D7DEC8BC1",
-    "Umator Email",
-    "Plugin for email management, sending, reading , deleting and moving emails", Author = "Umator", AuthorUri = "https://umator.com", DocumentationUri = "https://umator.com/plugins/email", SupportUri = "https://umator.com/plugins/email"
+    "Umator.Email",
+    "Plugin for email management, sending, reading , deleting and moving emails", 
+    Author = "Umator", AuthorUri = "https://umator.com", 
+    DocumentationUri = "https://umator.com/docs/uamtor-email/", SupportUri = "https://umator.com/support/forum/?view=forum&id=13"
 )]
